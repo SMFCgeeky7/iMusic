aa39b2629e5790daccdeb6df7af50e8b50011d13
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-02-17 12:20:03 -0400

    Configured the tab view

---
+++ iMusic/Screens/ArtistsView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistsView.swift
+//  iMusic
+//
+//  Created by Sebastian Fernandez Conde on 17/2/25.
+//
+
+import SwiftUI
+
+struct ArtistsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistsView()
+}
---
+++ iMusic/Screens/SongsView.swift
@@ -0,0 +1,18 @@
+//
+//  SongsView.swift
+//  iMusic
+//
+//  Created by Sebastian Fernandez Conde on 17/2/25.
+//
+
+import SwiftUI
+
+struct SongsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongsView()
+}
