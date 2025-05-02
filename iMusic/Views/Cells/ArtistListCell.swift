d2ce911cf51c00fbd254584c03b9b357582bc524
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-05-02 07:37:46 -0400

    - Created the basic titles for each screen - Created the according cells to each screen

---
+++ iMusic/Views/Cells/ArtistListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ArtistListCell.swift
+//  iMusic
+//
+//  Created by Sebastian Fernandez Conde on 28/4/25.
+//
+
+import Foundation
---
+++ iMusic/Views/Grids/AlbumGridCell.swift
@@ -0,0 +1,26 @@
+//
+//  AlbumListCell.swift
+//  iMusic
+//
+//  Created by Sebastian Fernandez Conde on 29/4/25.
+//
+
+import SwiftUI
+
+struct AlbumGridCell: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                
+            }
+            
+            Text("Album Name")
+                
+            Text("Artist Name")
+        }
+    }
+}
+
+#Preview {
+    AlbumListCell()
+}
